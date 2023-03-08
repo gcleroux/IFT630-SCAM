47249c09a861ff09ae9fdd8080458deb755c6dda
Guillaume Cleroux <guicleroux@icloud.com>
2023-03-08 14:22:33 -0500

    Ouvriers concurrent avec des channels

--- pkg/people/mayor.go
+++ pkg/people/mayor.go
@@ -12,27 +12,39 @@ func MayorHello() string {
 	return "Hello, Mayor!"
 }
 
-var villeContenu = []string{}
-
 func MayorStart(budget int, nbOuvrier int) int {
+	nbProjets := 0
 	plusPetitPrix := batiment.TrouveBatimentMoinsCher()
 
-	for budget > plusPetitPrix {
+	for i := 0; i < nbOuvrier; i++ {
+		go Ouvrier(batiment.Projets, batiment.Complets)
+	}
+
+	for budget >= plusPetitPrix {
 		batimentChoisi := rand.Intn(len(batiment.ChoixBatiments))
 		if budget > batiment.ChoixBatiments[batimentChoisi].PrixBatiment {
 			commande := batiment.ChoixBatiments[batimentChoisi]
 			budget = budget - commande.PrixBatiment
-			OuvrierConstruit(commande)
-			villeContenu = append(villeContenu, commande.NomBatiment)
+
+			nbProjets++
+			batiment.Projets <- commande
+
 		}
 	}
+	close(batiment.Projets)
+
+	for i := 0; i < nbProjets; i++ {
+		msg := <-batiment.Complets
+		fmt.Println(msg)
+	}
+	close(batiment.Complets)
 	return budget
 }
 
 func MayorEnd(budget int) {
 	fmt.Println("Le Mayor prend sa retraite avec un budget restant de " + strconv.Itoa(budget) + "$")
 	fmt.Println("La ville contient les bâtiments suivants:")
-	for i := 0; i < len(villeContenu); i++ {
-		fmt.Println(villeContenu[i])
+	for i := 0; i < len(batiment.VilleContenu); i++ {
+		fmt.Println(batiment.VilleContenu[i])
 	}
 }
