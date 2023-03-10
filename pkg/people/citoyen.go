bfdb4e0a548ef25034e4dd2633e6f4ca7ad039be
Olivier Cimon <olivier.cimon@usherbrooke.ca>
2023-03-10 11:38:59 -0500

    Correction bug #7

--- pkg/people/citoyen.go
+++ pkg/people/citoyen.go
@@ -40,11 +40,19 @@ func Population(idCitoyen int) {
 	for visiteCompletee == false {
 		installations := batiment.VilleContenu
 
-		if len(installations) < 2 {
+		if len(installations) < 2 { // Pour l'instant si on met < 1, les citoyens vont tous dans le même batîment (trop long avant d'en avoir un 2e)
 			time.Sleep(time.Millisecond * 500)
 		} else {
-			choix := rand.Intn(len(installations))
-			installation := batiment.ChoixBatiments[choix]
+			choix := installations[rand.Intn(len(installations))]
+
+			var index int
+			for i := range batiment.ChoixBatiments {
+				if batiment.ChoixBatiments[i].NomBatiment == choix {
+					index = i
+				}
+			}
+
+			installation := batiment.ChoixBatiments[index]
 			dureeVisite := installation.EffortBatiment
 			fmt.Println("Le citoyen", idCitoyen, "utilise les services offert par ", installation.NomBatiment)
 			time.Sleep(time.Millisecond * time.Duration(dureeVisite))
