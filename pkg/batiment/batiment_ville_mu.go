0f89b63f55a530ef0b91dfa1cf76d7fa03b26796
Olivier Cimon <olivier.cimon@usherbrooke.ca>
2023-04-17 09:55:07 -0400

    Ajout de commentaires

--- pkg/batiment/batiment_ville_mu.go
+++ pkg/batiment/batiment_ville_mu.go
@@ -4,29 +4,35 @@ import (
 	"sync"
 )
 
+// Structure d'exclusion mutuelle read/write pour gérer les batiments de la ville qui sont une partagés par différents threads.
+// sync.RWMutex permet la lecture simultané de plusieurs processus ou un seul processus en écriture.
 type BatimentVille struct {
 	BatimentsVille      []Batiment
 	BatimentsVilleMutex sync.RWMutex
 }
 
+// Ajouter un nouveau batiment
 func (batiments *BatimentVille) Append(nouveauBatiment Batiment) {
 	batiments.BatimentsVilleMutex.Lock()
 	defer batiments.BatimentsVilleMutex.Unlock()
 	batiments.BatimentsVille = append(batiments.BatimentsVille, nouveauBatiment)
 }
 
+// Retourne le nombre de batiments dans le tableau
 func (batiments *BatimentVille) Length() int {
 	batiments.BatimentsVilleMutex.RLock()
 	defer batiments.BatimentsVilleMutex.RUnlock()
 	return len(batiments.BatimentsVille)
 }
 
+// Retourne le batiment à l'index du tableau
 func (batiments *BatimentVille) Get(index int) Batiment {
 	batiments.BatimentsVilleMutex.RLock()
 	defer batiments.BatimentsVilleMutex.RUnlock()
 	return batiments.BatimentsVille[index]
 }
 
+// Retourne toute la liste des batiments
 func (batiments *BatimentVille) GetAll() []Batiment {
 	batimentsVille.BatimentsVilleMutex.RLock()
 	defer batimentsVille.BatimentsVilleMutex.RUnlock()
--- pkg/batiment/projets_mu.go
+++ pkg/batiment/projets_mu.go
@@ -4,41 +4,49 @@ import (
 	"sync"
 )
 
+// Structure d'exclusion mutuelle read/write pour gérer les proejts de la ville qui sont partagés par différents threads.
+// sync.RWMutex permet la lecture simultané de plusieurs processus ou un seul processus en écriture.
 type ProjetVille struct {
 	ProjetsVille      []Projet
 	ProjetsVilleMutex sync.RWMutex
 }
 
+// Ajouter un nouveau projet
 func (projets *ProjetVille) Append(nouveauProjet Projet) {
 	projets.ProjetsVilleMutex.Lock()
 	defer projets.ProjetsVilleMutex.Unlock()
 	projets.ProjetsVille = append(projets.ProjetsVille, nouveauProjet)
 }
 
+// Supprimer un projet à l'index indiqué
 func (projets *ProjetVille) Delete(index int) {
 	projets.ProjetsVilleMutex.Lock()
 	defer projets.ProjetsVilleMutex.Unlock()
 	projets.ProjetsVille = append(projets.ProjetsVille[:index], projets.ProjetsVille[index+1:]...)
 }
 
+// Retourne le nombre de projets dans le tableau
 func (projets *ProjetVille) Length() int {
 	projets.ProjetsVilleMutex.RLock()
 	defer projets.ProjetsVilleMutex.RUnlock()
 	return len(projets.ProjetsVille)
 }
 
+// Retourne le projet à l'index du tableau
 func (projets *ProjetVille) Get(index int) Projet {
 	projets.ProjetsVilleMutex.RLock()
 	defer projets.ProjetsVilleMutex.RUnlock()
 	return projets.ProjetsVille[index]
 }
 
+// Change la valeur d'un projet à l'index indiqué
 func (projets *ProjetVille) Set(index int, val Projet) {
 	projets.ProjetsVilleMutex.Lock()
 	defer projets.ProjetsVilleMutex.Unlock()
 	projets.ProjetsVille[index] = val
 }
 
+// Retourne toute la liste des projets
 func (projets *ProjetVille) GetAll() []Projet {
 	projets.ProjetsVilleMutex.RLock()
 	defer projets.ProjetsVilleMutex.RUnlock()
